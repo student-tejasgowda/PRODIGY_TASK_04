95ff89a55a8c97d9e53077417473244de751d2a9
student-tejasgowda <junjannas6364@gmail.com>
2024-06-15 12:28:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting me! I will get back to you soon.');
+});
